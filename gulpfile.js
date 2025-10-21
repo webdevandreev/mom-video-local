c14276394518c9b23e4cbe7d918a93143d03277d
webdevandreev <webdevandreev@gmail.com>
2025-10-21 21:59:27 +0300

    add gulp package

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
 const gulp = require('gulp');
 const rev = require('gulp-rev');
 const revRewrite = require('gulp-rev-rewrite');
-const revDelete = require('gulp-rev-delete-original');
 
 // Основная задача сборки
 gulp.task('build', function() {
     return gulp.src(['css/*.css', 'js/*.js'])
         .pipe(rev()) // Добавляем хеш к именам
-        .pipe(revDelete()) // Удаляем оригинальные файлы
         .pipe(gulp.dest('dist/')) // Сохраняем в dist
         .pipe(rev.manifest()) // Создаем manifest.json
         .pipe(gulp.dest('dist/'));
