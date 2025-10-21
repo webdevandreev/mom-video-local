a8d1ad2603c4ca7c270b3ebf35d2a3380da28c6a
webdevandreev <webdevandreev@gmail.com>
2025-10-21 22:19:58 +0300

    feat: complete Gulp build setup with cache busting

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,23 @@
 const gulp = require('gulp');
 const rev = require('gulp-rev');
 const revRewrite = require('gulp-rev-rewrite');
+const { rm } = require('fs/promises');
+
+// Очистка папки dist
+gulp.task('clean', async function() {
+    try {
+        await rm('dist', { recursive: true, force: true });
+    } catch (error) {
+        // Папки может не существовать - это ок
+    }
+});
 
 // Основная задача сборки
 gulp.task('build', function() {
     return gulp.src(['css/*.css', 'js/*.js'])
-        .pipe(rev()) // Добавляем хеш к именам
-        .pipe(gulp.dest('dist/')) // Сохраняем в dist
-        .pipe(rev.manifest()) // Создаем manifest.json
+        .pipe(rev())
+        .pipe(gulp.dest('dist/'))
+        .pipe(rev.manifest())
         .pipe(gulp.dest('dist/'));
 });
 
@@ -16,9 +26,9 @@ gulp.task('html', function() {
     const manifest = gulp.src('dist/rev-manifest.json');
     
     return gulp.src('index.html')
-        .pipe(revRewrite({ manifest })) // Обновляем ссылки в HTML
+        .pipe(revRewrite({ manifest }))
         .pipe(gulp.dest('dist/'));
 });
 
 // Задача по умолчанию
-gulp.task('default', gulp.series('build', 'html'));
+gulp.task('default', gulp.series('clean', 'build', 'html'));
