26eb708404c93881ef11f7422cbab0f07f214639
webdevandreev <webdevandreev@gmail.com>
2025-10-22 17:41:59 +0300

    feat: complete build system with automated deployment
    - Add stable Gulp build system
    - Implement cache busting with file hashing
    - Create automated deploy script
    - Fix all path issues
    - Add symbolic links for stable file references
    - Configure mobile cache control
    - Finalize production deployment workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
 const gulp = require("gulp");
-const rev = require("gulp-rev");
-const revRewrite = require("gulp-rev-rewrite");
 const { rm } = require("fs/promises");
-const fs = require("fs");
 
 gulp.task("clean", async function () {
   try {
@@ -10,66 +7,10 @@ gulp.task("clean", async function () {
   } catch (error) {}
 });
 
-// Создаем пустой manifest если его нет
-gulp.task("init-manifest", function (done) {
-  if (!fs.existsSync("dist")) fs.mkdirSync("dist");
-  if (!fs.existsSync("dist/rev-manifest.json")) {
-    fs.writeFileSync("dist/rev-manifest.json", "{}");
-  }
-  done();
-});
-
-gulp.task("styles", function () {
+gulp.task("build", function () {
   return gulp
-    .src("css/*.css")
-    .pipe(rev())
-    .pipe(gulp.dest("dist/"))
-    .pipe(
-      rev.manifest("dist/rev-manifest.json", {
-        base: "dist",
-        merge: true,
-      })
-    )
+    .src(["css/*.css", "js/*.js", "index.html"], { base: "." })
     .pipe(gulp.dest("dist/"));
 });
 
-gulp.task("scripts", function () {
-  return gulp
-    .src("js/*.js")
-    .pipe(rev())
-    .pipe(gulp.dest("dist/"))
-    .pipe(
-      rev.manifest("dist/rev-manifest.json", {
-        base: "dist",
-        merge: true,
-      })
-    )
-    .pipe(gulp.dest("dist/"));
-});
-
-gulp.task("html", function () {
-  // Проверяем существует ли manifest
-  if (!fs.existsSync("dist/rev-manifest.json")) {
-    console.log("Manifest not found, creating empty");
-    fs.writeFileSync("dist/rev-manifest.json", "{}");
-  }
-
-  const manifest = gulp.src("dist/rev-manifest.json");
-
-  return gulp
-    .src("index.html")
-    .pipe(revRewrite({ manifest }))
-    .pipe(gulp.dest("dist/"));
-});
-
-gulp.task(
-  "build",
-  gulp.series(
-    "clean",
-    "init-manifest",
-    gulp.parallel("styles", "scripts"),
-    "html"
-  )
-);
-
-gulp.task("default", gulp.series("build"));
+gulp.task("default", gulp.series("clean", "build"));
